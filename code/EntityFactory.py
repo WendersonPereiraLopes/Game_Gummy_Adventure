a392b724676ae24c59d116f1e6c4998e93dcc644
WendersonPereiraLopes <wendersonlopes.softeng@gmail.com>
2026-06-28 22:17:13 -0300

    update

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -11,5 +11,6 @@ class EntityFactory:
                 list_bg = []
                 list_bg.append(Background('LevelBg', (0,0), (WIN_WIDTH, WIN_HEIGHT)))
                 list_bg.append(Background('plataforma01Bg', (0, 410), (WIN_WIDTH / 4,50)))
-
+                list_bg.append(Background('aguaBg', (200, 410), (WIN_WIDTH / 4,50)))
+               
                 return list_bg
